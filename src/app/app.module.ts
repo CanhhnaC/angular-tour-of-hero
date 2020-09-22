b30477fa4ca3c1d97ca0b761a215b05c485468b4
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-09-22 09:58:09 +0700

    5, App In-app Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,22 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, RouterModule],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
